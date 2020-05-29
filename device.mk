eb391e9cffcbbb0a56f77a6958b3ade5ddebdbbf
Jarl-Penguin <jarlpenguin@outlook.com>
2020-05-29 21:26:21 +0300

    james: Decommonize egis keylayouts
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ied93016eeb0ed29c32b18b957030de73f1ad57e4

--- device.mk
+++ device.mk
@@ -36,6 +36,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/unavail.android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/unavail.android.hardware.fingerprint.xml \
     $(LOCAL_PATH)/configs/permissions/unavail.android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/unavail.android.hardware.sensor.gyroscope.xml
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-egis.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-egis.kl
+
+# IDC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinput-egis.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-egis.idc
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.device.rc
